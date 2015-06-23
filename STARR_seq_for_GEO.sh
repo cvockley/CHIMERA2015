dbb940939497287bb636073d7177fe23f6800971
Christopher Vockley <cv42@hardac-node09-1.genome.duke.edu>
2015-06-23 13:38:23 -0400

    First commit

---
+++ STARR_seq_for_GEO.sh
@@ -0,0 +1,62 @@
+#! /bin/bash
+cat /data/reddylab/Flowcells/Project_Vockley_743_131102/CV19-1_S1_L001_R1_001.fastq /data/reddylab/Flowcells/Project_Vockley_744_131108/CV19-1_S1_L001_R1_001.fastq -> ./STARR_seq__6BACS_3hr_EtOH_Rep1_Read1.fastq &
+
+cat /data/reddylab/Flowcells/Project_Vockley_743_131102/CV19-1_S1_L001_R2_001.fastq /data/reddylab/Flowcells/Project_Vockley_744_131108/CV19-1_S1_L001_R2_001.fastq -> ./STARR_seq__6BACS_3hr_EtOH_Rep1_Read2.fastq &
+
+cat /data/reddylab/Flowcells/Project_Vockley_743_131102/CV19-2_S2_L001_R1_001.fastq /data/reddylab/Flowcells/Project_Vockley_744_131108/CV19-2_S2_L001_R1_001.fastq -> ./STARR_seq__6BACS_3hr_EtOH_Rep2_Read1.fastq &
+
+cat /data/reddylab/Flowcells/Project_Vockley_743_131102/CV19-2_S2_L001_R2_001.fastq /data/reddylab/Flowcells/Project_Vockley_744_131108/CV19-2_S2_L001_R2_001.fastq -> ./STARR_seq__6BACS_3hr_EtOH_Rep2_Read2.fastq &
+
+cat /data/reddylab/Flowcells/Project_Vockley_743_131102/CV19-3_S3_L001_R1_001.fastq /data/reddylab/Flowcells/Project_Vockley_744_131108/CV19-3_S3_L001_R1_001.fastq -> ./STARR_seq__6BACS_3hr_EtOH_Rep3_Read1.fastq &	
+
+cat /data/reddylab/Flowcells/Project_Vockley_743_131102/CV19-3_S3_L001_R2_001.fastq /data/reddylab/Flowcells/Project_Vockley_744_131108/CV19-3_S3_L001_R2_001.fastq -> ./STARR_seq__6BACS_3hr_EtOH_Rep3_Read2.fastq &
+
+cat /data/reddylab/Flowcells/Project_Vockley_743_131102/CV19-4_S4_L001_R1_001.fastq /data/reddylab/Flowcells/Project_Vockley_744_131108/CV19-4_S4_L001_R1_001.fastq -> ./STARR_seq__6BACS_3hr_100nM_DEX_Rep1_Read1.fastq &	
+
+cat /data/reddylab/Flowcells/Project_Vockley_743_131102/CV19-4_S4_L001_R2_001.fastq /data/reddylab/Flowcells/Project_Vockley_744_131108/CV19-4_S4_L001_R2_001.fastq -> ./STARR_seq__6BACS_3hr_100nM_DEX_Rep1_Read2.fastq &	
+
+cat /data/reddylab/Flowcells/Project_Vockley_743_131102/CV19-5_S5_L001_R1_001.fastq /data/reddylab/Flowcells/Project_Vockley_744_131108/CV19-5_S5_L001_R1_001.fastq -> ./STARR_seq__6BACS_3hr_100nM_DEX_Rep2_Read1.fastq &	
+
+cat /data/reddylab/Flowcells/Project_Vockley_743_131102/CV19-5_S5_L001_R2_001.fastq /data/reddylab/Flowcells/Project_Vockley_744_131108/CV19-5_S5_L001_R2_001.fastq -> ./STARR_seq__6BACS_3hr_100nM_DEX_Rep2_Read2.fastq &	
+
+cat /data/reddylab/Flowcells/Project_Vockley_743_131102/CV19-6_S6_L001_R1_001.fastq /data/reddylab/Flowcells/Project_Vockley_744_131108/CV19-6_S6_L001_R1_001.fastq -> ./STARR_seq__6BACS_3hr_100nM_DEX_Rep3_Read1.fastq &	
+
+cat /data/reddylab/Flowcells/Project_Vockley_743_131102/CV19-6_S6_L001_R2_001.fastq /data/reddylab/Flowcells/Project_Vockley_744_131108/CV19-6_S6_L001_R2_001.fastq -> ./STARR_seq__6BACS_3hr_100nM_DEX_Rep3_Read2.fastq &
+	
+
+
+
+
+cp /data/reddylab/Flowcells/ChrisVockley/Project_Vockley_0001_150615/gcmod3-dex1_S1_L001_R1_001.fastq  ./GC_STARRSEQ_Mod3_3hr_100nM_DEX_rep1_read1.fastq &
+cp /data/reddylab/Flowcells/ChrisVockley/Project_Vockley_0001_150615/gcmod3-dex1_S1_L001_R2_001.fastq  ./GC_STARRSEQ_Mod3_3hr_100nM_DEX_rep1_read2.fastq &
+cp /data/reddylab/Flowcells/ChrisVockley/Project_Vockley_0001_150615/gcmod3-dex2_S2_L001_R1_001.fastq  ./GC_STARRSEQ_Mod3_3hr_100nM_DEX_rep2_read1.fastq &
+cp /data/reddylab/Flowcells/ChrisVockley/Project_Vockley_0001_150615/gcmod3-dex2_S2_L001_R2_001.fastq  ./GC_STARRSEQ_Mod3_3hr_100nM_DEX_rep2_read2.fastq &
+cp /data/reddylab/Flowcells/ChrisVockley/Project_Vockley_0001_150615/gcmod3-dex3_S3_L001_R1_001.fastq  ./GC_STARRSEQ_Mod3_3hr_100nM_DEX_rep3_read1.fastq &
+cp /data/reddylab/Flowcells/ChrisVockley/Project_Vockley_0001_150615/gcmod3-dex3_S3_L001_R2_001.fastq  ./GC_STARRSEQ_Mod3_3hr_100nM_DEX_rep3_read2.fastq &
+cp /data/reddylab/Flowcells/ChrisVockley/Project_Vockley_0001_150615/gcmod3-etoh1_S4_L001_R1_001.fastq  ./GC_STARRSEQ_Mod3_3hr_EtOH_rep1_read1.fastq &
+cp /data/reddylab/Flowcells/ChrisVockley/Project_Vockley_0001_150615/gcmod3-etoh1_S4_L001_R2_001.fastq  ./GC_STARRSEQ_Mod3_3hr_EtOH_rep1_read2.fastq &
+cp /data/reddylab/Flowcells/ChrisVockley/Project_Vockley_0001_150615/gcmod3-etoh2_S5_L001_R1_001.fastq  ./GC_STARRSEQ_Mod3_3hr_EtOH_rep2_read1.fastq &
+cp /data/reddylab/Flowcells/ChrisVockley/Project_Vockley_0001_150615/gcmod3-etoh2_S5_L001_R2_001.fastq  ./GC_STARRSEQ_Mod3_3hr_EtOH_rep2_read2.fastq &
+cp /data/reddylab/Flowcells/ChrisVockley/Project_Vockley_0001_150615/gcmod3-etoh3_S6_L001_R1_001.fastq  ./GC_STARRSEQ_Mod3_3hr_EtOH_rep3_read1.fastq &
+cp /data/reddylab/Flowcells/ChrisVockley/Project_Vockley_0001_150615/gcmod3-etoh3_S6_L001_R2_001.fastq  ./GC_STARRSEQ_Mod3_3hr_EtOH_rep3_read2.fastq &
+
+cp /data/reddylab/Flowcells/ChrisVockley/Project_Vockley_0002_150615/gcmod1-dex1_S1_L001_R1_001.fastq  ./GC_STARRSEQ_Mod1_3hr_100nM_DEX_rep1_read1.fastq &
+cp /data/reddylab/Flowcells/ChrisVockley/Project_Vockley_0002_150615/gcmod1-dex1_S1_L001_R2_001.fastq  ./GC_STARRSEQ_Mod1_3hr_100nM_DEX_rep1_read2.fastq &
+cp /data/reddylab/Flowcells/ChrisVockley/Project_Vockley_0002_150615/gcmod1-dex2_S2_L001_R1_001.fastq  ./GC_STARRSEQ_Mod1_3hr_100nM_DEX_rep2_read1.fastq &
+cp /data/reddylab/Flowcells/ChrisVockley/Project_Vockley_0002_150615/gcmod1-dex2_S2_L001_R2_001.fastq  ./GC_STARRSEQ_Mod1_3hr_100nM_DEX_rep2_read2.fastq &
+cp /data/reddylab/Flowcells/ChrisVockley/Project_Vockley_0002_150615/gcmod1-dex3_S3_L001_R1_001.fastq  ./GC_STARRSEQ_Mod1_3hr_100nM_DEX_rep3_read1.fastq &
+cp /data/reddylab/Flowcells/ChrisVockley/Project_Vockley_0002_150615/gcmod1-dex3_S3_L001_R2_001.fastq  ./GC_STARRSEQ_Mod1_3hr_100nM_DEX_rep3_read2.fastq &
+cp /data/reddylab/Flowcells/ChrisVockley/Project_Vockley_0002_150615/gcmod1-etoh1_S4_L001_R1_001.fastq  ./GC_STARRSEQ_Mod1_3hr_EtOH_rep1_read1.fastq &
+cp /data/reddylab/Flowcells/ChrisVockley/Project_Vockley_0002_150615/gcmod1-etoh1_S4_L001_R2_001.fastq  ./GC_STARRSEQ_Mod1_3hr_EtOH_rep1_read2.fastq &
+cp /data/reddylab/Flowcells/ChrisVockley/Project_Vockley_0002_150615/gcmod1-etoh2_S5_L001_R1_001.fastq  ./GC_STARRSEQ_Mod1_3hr_EtOH_rep2_read1.fastq &
+cp /data/reddylab/Flowcells/ChrisVockley/Project_Vockley_0002_150615/gcmod1-etoh2_S5_L001_R2_001.fastq  ./GC_STARRSEQ_Mod1_3hr_EtOH_rep2_read2.fastq &
+cp /data/reddylab/Flowcells/ChrisVockley/Project_Vockley_0002_150615/gcmod1-etoh3_S6_L001_R1_001.fastq  ./GC_STARRSEQ_Mod1_3hr_EtOH_rep3_read1.fastq &
+cp /data/reddylab/Flowcells/ChrisVockley/Project_Vockley_0002_150615/gcmod1-etoh3_S6_L001_R2_001.fastq  ./GC_STARRSEQ_Mod1_3hr_EtOH_rep3_read2.fastq &
+
+
+
+cp /data/reddylab/Flowcells/ChrisVockley/Project_Vockley_2501_150528/GC-mod1_S1_L001_R1_001.fastq  ./GC_STARRSEQ_Mod1_input_plasmids_read1.fastq &
+cp /data/reddylab/Flowcells/ChrisVockley/Project_Vockley_2501_150528/GC-mod1_S1_L001_R2_001.fastq  ./GC_STARRSEQ_Mod1_input_plasmids_read2.fastq &
+
+cp /data/reddylab/Flowcells/ChrisVockley/Project_Vockley_2501_150528/GC-mod3_S3_L001_R1_001.fastq  ./GC_STARRSEQ_Mod3_input_plasmids_read1.fastq &
+cp /data/reddylab/Flowcells/ChrisVockley/Project_Vockley_2501_150528/GC-mod3_S3_L001_R2_001.fastq  ./GC_STARRSEQ_Mod3_input_plasmids_read2.fastq &
